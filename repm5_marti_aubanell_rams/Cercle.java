0216cc8e6057938b92410c7745a5cbd96110fa99
maubanell <marti.gnd@gmail.com>
2019-04-01 15:30:32 +0200

    cercle funcions

--- repm5_marti_aubanell_rams/Cercle.java
+++ repm5_marti_aubanell_rams/Cercle.java
@@ -7,12 +7,12 @@ public class Cercle{
 		this.y=yy;
 	}
 	Cercle(){
-		this.y=1;
-		this.x=2;
+		this.y=3;
+		this.x=3;
 	}
 	Cercle(Cercle cerc){
-		cerc.y=this.y;
-		cerc.x=this.x;
+		cerc.y=cerc.x;
+		cerc.x=cerc.y;
 	}
 	public String toString(){
 		return x + " " + y;
@@ -25,4 +25,14 @@ public class Cercle{
 			return false;
 		}
 	}
+        public void setCercle(float xx, float yy){
+            this.x = xx;
+            this.y = yy;
+        }
+        public float getCercleX(){
+            return this.x;
+        }
+        public float getCercleY(){
+            return this.y;
+        }
 }
